ee31e029f3bf816d4338a8d9ede0619d6929d8e3
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-04-06 18:15:53 +0200

    Updates

---
+++ precache-manifest.fd5c5fb99c1af5b33471f2d2355f2718.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9019382d41e1a87354525265a20b42e8",
+    "url": "//tyroniua.github.io/x-culture/index.html"
+  },
+  {
+    "revision": "458a62290b30f43c2933",
+    "url": "//tyroniua.github.io/x-culture/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "075aff940daac291285a",
+    "url": "//tyroniua.github.io/x-culture/static/css/main.318a61c1.chunk.css"
+  },
+  {
+    "revision": "458a62290b30f43c2933",
+    "url": "//tyroniua.github.io/x-culture/static/js/2.97ccbff2.chunk.js"
+  },
+  {
+    "revision": "0a3ab8185718fe3bbf9eb836ac22b0bf",
+    "url": "//tyroniua.github.io/x-culture/static/js/2.97ccbff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075aff940daac291285a",
+    "url": "//tyroniua.github.io/x-culture/static/js/main.e0306afe.chunk.js"
+  },
+  {
+    "revision": "5558a5b90b3b5158ba3f",
+    "url": "//tyroniua.github.io/x-culture/static/js/runtime-main.44660c6e.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "//tyroniua.github.io/x-culture/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "3ce8a12f950136977f2b82aebdc7e7fc",
+    "url": "//tyroniua.github.io/x-culture/static/media/imgbusiness.3ce8a12f.png"
+  },
+  {
+    "revision": "bbb7aed642d4cbc6094c0abb8fd33047",
+    "url": "//tyroniua.github.io/x-culture/static/media/imgresearch.bbb7aed6.png"
+  },
+  {
+    "revision": "091b30c1ba792249da004a0945c65be7",
+    "url": "//tyroniua.github.io/x-culture/static/media/imgtraining.091b30c1.png"
+  }
+]);
