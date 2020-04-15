042f45ae87d307733245e4371359c30c032cd233
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-04-15 15:54:31 +0200

    Updates

---
+++ precache-manifest.e432c00686f9a20a59dbac9cd0edab63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb6f09d2d9a654de25ff8d1fe469fbc",
+    "url": "/TyroniUA/x-culture/index.html"
+  },
+  {
+    "revision": "b464ca2fc3ac62374a87",
+    "url": "/TyroniUA/x-culture/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "375e9cf45c59f7c5f4ed",
+    "url": "/TyroniUA/x-culture/static/css/main.f34c0cca.chunk.css"
+  },
+  {
+    "revision": "b464ca2fc3ac62374a87",
+    "url": "/TyroniUA/x-culture/static/js/2.341cc15c.chunk.js"
+  },
+  {
+    "revision": "0a3ab8185718fe3bbf9eb836ac22b0bf",
+    "url": "/TyroniUA/x-culture/static/js/2.341cc15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375e9cf45c59f7c5f4ed",
+    "url": "/TyroniUA/x-culture/static/js/main.1f702583.chunk.js"
+  },
+  {
+    "revision": "705d01aa5520f08bc3f7",
+    "url": "/TyroniUA/x-culture/static/js/runtime-main.f7410161.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/TyroniUA/x-culture/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "3ce8a12f950136977f2b82aebdc7e7fc",
+    "url": "/TyroniUA/x-culture/static/media/imgbusiness.3ce8a12f.png"
+  },
+  {
+    "revision": "bbb7aed642d4cbc6094c0abb8fd33047",
+    "url": "/TyroniUA/x-culture/static/media/imgresearch.bbb7aed6.png"
+  },
+  {
+    "revision": "091b30c1ba792249da004a0945c65be7",
+    "url": "/TyroniUA/x-culture/static/media/imgtraining.091b30c1.png"
+  }
+]);
